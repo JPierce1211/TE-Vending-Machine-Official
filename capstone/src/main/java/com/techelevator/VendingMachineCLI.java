d3ef8b82a52907924910a85a23ce79eb318b2832
afreen kazi <dr.afreen.kazi@gmail.com>
2023-06-08 11:27:46 -0400

    Capstone 1

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,9 +1,11 @@
 package com.techelevator;
 
+import com.techelevator.view.Console;
 import com.techelevator.view.Menu;
 
 public class VendingMachineCLI {
 
+	private static final String WELCOME_MESSAGE = "Welcome to Vendo-Matic 800";
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
@@ -15,6 +17,9 @@ public class VendingMachineCLI {
 	}
 
 	public void run() {
+
+		Console console = new Console();
+		console.displayMessage(WELCOME_MESSAGE);
 		while (true) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
